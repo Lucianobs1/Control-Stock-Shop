b6368221cb3f79f67bff160308e713d0b7a8ab91
Seu NomeLuciano <lucianotetsu@gmail.com>
2019-02-20 01:01:27 -0300

    Add project files.

---
+++ ControlStock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ControlStock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
