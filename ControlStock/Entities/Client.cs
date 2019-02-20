218b1a90c3150426e1f2e62bedc7e7a7bb28f89d
Seu NomeLuciano <lucianotetsu@gmail.com>
2019-02-20 01:39:06 -0300

    Adicionando e implementando a classe client

---
+++ ControlStock/Entities/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlStock.Entities
+{
+    class Client
+    {
+        public string Nome { get; set; }
+        public double Card { get; set; }
+        public double Money { get; set; }
+        List<Product> Products { get; set; } = new List<Product>();
+
+        public Client()
+        {
+
+        }
+
+        public Client(string nome, double card, double money)
+        {
+            Nome = nome;
+            Card = card;
+            Money = money;
+        }
+
+        public void addProduct(Product product) {
+
+            Products.Add(product);
+        }
+
+        public void RemoveProduct(Product product)
+        {
+
+            Products.Remove(product);
+        }
+    }
+}
